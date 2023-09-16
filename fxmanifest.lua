4afd5380fac62049386adef0052b151a8550253e
simone.exe <45904818+404error-dev@users.noreply.github.com>
2023-09-16 02:41:51 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'simone.exe.'
+description 'eagle_notifiche'
+version '1.0.0'
+
+client_script 'client.lua'
+
+ui_page('html/index.html')
+
+files {
+    'html/index.html',
+    'html/ui.css',
+    'html/ui.js',
+    'html/audio.mp3'
+}
